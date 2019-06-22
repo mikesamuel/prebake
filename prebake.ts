76ea55573a8809e7ce571b9d2cdd9c443a559d64
Mike Samuel <mikesamuel@gmail.com>
2019-06-22 15:35:15 -0400

    prebake() includes the moduleSet in its output

--- prebake.ts
+++ prebake.ts
@@ -11,6 +11,7 @@ import { Gatherer } from './src/gatherer';
 import { Rewriter } from './src/rewriter';
 
 export interface PrebakeResult {
+  moduleSet: ModuleSet;
   modules: Map<ModuleKey, Module>;
   specifierToId: Map<string, ModuleId>;
 }
@@ -47,7 +48,7 @@ export class Prebakery {
    * promise to a map that maps the input module ids to prebaked modules.
    * The returned map will also include entries for any dependencies.
    */
-  prebake(...moduleSpecifiers: string[]): Promise<PrebakeResult> {
+  async prebake(...moduleSpecifiers: string[]): Promise<PrebakeResult> {
     const fetchContext: FetchContext = {
       level: 'info',
       moduleId: this.baseModuleId,
@@ -57,44 +58,37 @@ export class Prebakery {
     const specifierToId: Map<string, ModuleId> = new Map();
     const resolutionPromises: Promise<Module>[] = [];
     for (const moduleSpecifier of moduleSpecifiers) {
-      const p = this.moduleSet.fetch(moduleSpecifier, fetchContext);
-      p.then((m: Module) => {
-        specifierToId.set(moduleSpecifier, m.id);
-      });
+      const p = this.moduleSet.fetch(moduleSpecifier, fetchContext).then(
+        async (m: Module): Promise<Module> => {
+          specifierToId.set(moduleSpecifier, m.id);
+          return m;
+        });
       resolutionPromises.push(p);
     }
 
-    return new Promise((resolve: (r: PrebakeResult) => void) => {
-      Promise.all(resolutionPromises).then(
-        (resolvedModules: Module[]) => {
-          const modules: Map<ModuleKey, Module> = new Map();
+    const resolvedModules: Module[] = await Promise.all(resolutionPromises);
 
-          const finishPromises = [];
-          for (const m of resolvedModules) {
-            if (m.errors && m.errors.length) {
-              modules.set(m.id.key(), m);
-            } else {
-              finishPromises.push(
-                this.moduleSet.onPromotionTo(m, RewrittenModule.prototype.constructor));
-            }
-          }
+    const modules: Map<ModuleKey, Module> = new Map();
 
-          Promise.all(finishPromises).then(
-            (finishedModules: Module[]) => {
-              for (const m of finishedModules) {
-                modules.set(m.id.key(), m);
-              }
-              // TODO: transitive dependencies of rewritten modules.
-              resolve({
-                modules,
-                specifierToId,
-              });
-            });
-        },
-        (err) => {
-          // TODO
-          console.error(err);
-        });
-    });
+    const finishPromises = [];
+    for (const m of resolvedModules) {
+      if (m.errors && m.errors.length) {
+        modules.set(m.id.key(), m);
+      } else {
+        finishPromises.push(
+          this.moduleSet.onPromotionTo(m, RewrittenModule.prototype.constructor));
+      }
+    }
+
+    const finishedModules: Module[] = await Promise.all(finishPromises);
+    for (const m of finishedModules) {
+      modules.set(m.id.key(), m);
+    }
+
+    return {
+      moduleSet: this.moduleSet,
+      modules,
+      specifierToId,
+    };
   }
 }
