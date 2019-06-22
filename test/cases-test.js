ec1a4001d285ac1e0f9a364e2e8128c420fc688e
Mike Samuel <mikesamuel@gmail.com>
2019-06-22 15:41:48 -0400

    Fleshed out test harness.
    The test harness now extracts data from prebake()'s output and
    compares it to a golden file.
    
    Only test still broken.  Disabled for now.

--- test/cases-test.js
+++ test/cases-test.js
@@ -3,11 +3,13 @@
 const fs = require('fs');
 const path = require('path');
 const { URL, pathToFileURL } = require('url');
+const { promisify } = require('util');
 const { expect } = require('chai');
-const { describe, it } = require('mocha');
+const { describe, it, xit } = require('mocha');
 
 const { Prebakery } = require('../lib/prebake.js');
 const { fileSystemFetcher } = require('../lib/src/fetcher.js');
+const { RewrittenModule } = require('../lib/src/module.js');
 const { CanonModuleId, TentativeModuleId } = require('../lib/src/module-id.js');
 
 describe('cases', () => {
@@ -16,35 +18,62 @@ describe('cases', () => {
     const caseDir = path.join(caseRoot, subDir);
 
     const inpJsPath = path.join(caseDir, 'inp.js');
+    const wantJsonPath = path.join(caseDir, 'want.json');
+
     if (fs.existsSync(inpJsPath)) {
-      it(subDir, (done) => {
+      xit(subDir, async () => {
         const logEntries = [];
         const cassandra = (event) => {
           logEntries.push(event);
         };
-        const prebakery = new Prebakery(
-          fileSystemFetcher,
-          cassandra,
-          new CanonModuleId(
-            pathToFileURL(caseDir),
-            pathToFileURL(fs.realpathSync.native(caseDir))));
-        const parts = inpJsPath.split(path.sep).filter(Boolean);
-        const inpModuleSpecifier = `/${ parts.map(encodeURIComponent).join('/') }`;
-        prebakery.prebake(inpModuleSpecifier).then(
-          ({ modules, specifierToId }) => {
-            const got = { specifierToId, logEntries, modules: {} };
-            for (const [key, value] of modules.entries()) {
-              got.modules[key] = value.source || value.errors;
-            }
-            // TODO: compare got to golden
-            done();
-          },
-          (error) => {
+        let passed = false;
+        try {
+          const prebakery = new Prebakery(
+            fileSystemFetcher,
+            cassandra,
+            new CanonModuleId(
+              pathToFileURL(caseDir),
+              pathToFileURL(fs.realpathSync.native(caseDir))));
+          const parts = inpJsPath.split(path.sep).filter(Boolean);
+          const inpModuleSpecifier = `/${ parts.map(encodeURIComponent).join('/') }`;
+          const result = await prebakery.prebake(inpModuleSpecifier);
+          const specifierToId = {};
+          for (const [spec, id] of result.specifierToId) {
+            specifierToId[spec] = id;
+          }
+          const got = { specifierToId, logEntries, modules: {} };
+          const finishPromises = [];
+          for (const [key, module] of result.modules.entries()) {
+            const moduleData = {
+              source: module.source || undefined,
+              errors: module.errors || undefined,
+            };
+            got.modules[key] = moduleData;
+            finishPromises.push(
+              // TODO: glean target module stage from test config.
+              result.moduleSet.onPromotionTo(module, RewrittenModule).then(
+                async (finishedModule) => {
+                  if (finishedModule.errors) {
+                    moduleData.errors = finishedModule.errors;
+                  } else {
+                    moduleData.deps = finishedModule.deps || [];
+                    moduleData.rdeps = finishedModule.rdeps || [];
+                  }
+                  return finishedModule;
+                }));
+          }
+          await Promise.all(finishPromises);
+
+          const expected = await promisify(fs.readFile)(wantJsonPath, { encoding: 'utf-8' });
+          expect(JSON.stringify(got, null, 2))
+            .to.equal(JSON.stringify(JSON.parse(expected), null, 2));
+        } finally {
+          if (!passed) {
             for (const { level, line, message, moduleId } of logEntries) {
               console.log(`${ moduleId }:${ line }:${ level }: ${ message }`);
             }
-            done(error);
-          });
+          }
+        }
       });
     }
   }
