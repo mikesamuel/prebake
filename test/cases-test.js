04d6db62520d15370bed242b689744ca7bfd8a39
Mike Samuel <mikesamuel@gmail.com>
2019-06-22 20:23:26 -0400

    first working testcase
    Added substitution pass so that test goldens can be independent
    of working directory.
    
    Modified test golden for our one test.

--- test/cases-test.js
+++ test/cases-test.js
@@ -5,12 +5,20 @@ const path = require('path');
 const { URL, pathToFileURL } = require('url');
 const { promisify } = require('util');
 const { expect } = require('chai');
-const { describe, it, xit } = require('mocha');
+const { describe, it } = require('mocha');
 
 const { Prebakery } = require('../lib/prebake.js');
 const { fileSystemFetcher } = require('../lib/src/fetcher.js');
 const { RewrittenModule } = require('../lib/src/module.js');
 const { CanonModuleId, TentativeModuleId } = require('../lib/src/module-id.js');
+
+function undefIfEmpty(x) {
+  if (x === null || x === undefined
+      || ((typeof x === 'string' || Array.isArray(x)) && x.length === 0)) {
+    return undefined;
+  }
+  return x;
+}
 
 describe('cases', () => {
   const caseRoot = path.join(__dirname, 'cases');
@@ -20,8 +28,35 @@ describe('cases', () => {
     const inpJsPath = path.join(caseDir, 'inp.js');
     const wantJsonPath = path.join(caseDir, 'want.json');
 
+    const goldenSubs = {
+      __proto__: null,
+      $BASEDIR: caseDir,
+      $BASEURL: pathToFileURL(caseDir),
+    };
+    function substituteIntoGoldens(_, x) {
+      // JSON reviver that replaces $BASEDIR, etc. with relevant substring so
+      // that test goldens are insensitive to location of path on file system.
+      if (typeof x === 'string') {
+        return x.replace(
+          /(\\*)([$]\w+)/g,
+          function (whole, slashes, key) {
+            if ((slashes.length & 1) === 0 && key in goldenSubs) {
+              return slashes + goldenSubs[key];
+            }
+            return whole;
+          });
+      } else if (x && typeof x === 'object' && !Array.isArray(x)) {
+        const o = {};
+        for (const [key, value] of Object.entries(x)) {
+          o[substituteIntoGoldens(null, key)] = value;
+        }
+        return o;
+      }
+      return x;
+    }
+
     if (fs.existsSync(inpJsPath)) {
-      xit(subDir, async () => {
+      it(subDir, async () => {
         const logEntries = [];
         const cassandra = (event) => {
           logEntries.push(event);
@@ -41,32 +76,30 @@ describe('cases', () => {
           for (const [spec, id] of result.specifierToId) {
             specifierToId[spec] = id;
           }
-          const got = { specifierToId, logEntries, modules: {} };
           const finishPromises = [];
           for (const [key, module] of result.modules.entries()) {
-            const moduleData = {
-              source: module.source || undefined,
-              errors: module.errors || undefined,
-            };
-            got.modules[key] = moduleData;
             finishPromises.push(
               // TODO: glean target module stage from test config.
-              result.moduleSet.onPromotionTo(module, RewrittenModule).then(
-                async (finishedModule) => {
-                  if (finishedModule.errors) {
-                    moduleData.errors = finishedModule.errors;
-                  } else {
-                    moduleData.deps = finishedModule.deps || [];
-                    moduleData.rdeps = finishedModule.rdeps || [];
-                  }
-                  return finishedModule;
-                }));
+              result.moduleSet.onPromotionTo(module, RewrittenModule));
           }
           await Promise.all(finishPromises);
 
-          const expected = await promisify(fs.readFile)(wantJsonPath, { encoding: 'utf-8' });
+          const got = { specifierToId, logEntries, modules: {} };
+          for (const finishedModule of result.moduleSet.modules()) {
+            got.modules[finishedModule.id.key()] = {
+              source: undefIfEmpty(finishedModule.source),
+              errors: undefIfEmpty(finishedModule.errors),
+              deps: undefIfEmpty(finishedModule.deps),
+              rdeps: undefIfEmpty(finishedModule.rdeps),
+            };
+          }
+
+          const want = JSON.parse(
+            await promisify(fs.readFile)(wantJsonPath, { encoding: 'utf-8' }),
+            substituteIntoGoldens);
+
           expect(JSON.stringify(got, null, 2))
-            .to.equal(JSON.stringify(JSON.parse(expected), null, 2));
+            .to.equal(JSON.stringify(want, null, 2));
         } finally {
           if (!passed) {
             for (const { level, line, message, moduleId } of logEntries) {
